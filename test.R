707cdce0809e5fe251e41916b9a8669d43154a1f
Martin Lavoie <martin21skifond@gmail.com>
2015-07-08 14:06:57 -0400

    My commit comments

--- test.R
+++ test.R
@@ -34,3 +34,27 @@ import.list <- llply(filenames, read.table, header=TRUE, sep=",",na.strings="NA"
 Road <- readShapeLines("/Users/drisk/Desktop/nrn_rrn_ns_shp_en/NRN_NS_11_0_ROADSEG.shp")
 proj4string(Road) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
 Road_pc <- spTransform(Road, CRS = CRS("+init=epsg:2957")) #
+#
+#
+############################################################################################
+############################################################################################
+#This script is to apply RUNMIN on Picarro 2201 drive-around datasets and calculate e-ratios based on RUNMINS, and normal GLOBAL mins
+#Written by Dave Risk and Martin Lavoie, October 20 2014
+#
+#The script expects 
+#     1. A Drive-around file already processed#####
+#
+#The script does the following:
+#      A. Interpolates Picarro output to 1 second resolution in case there are gaps and to apply the RUNMIN interval correctly
+#      B. Puts a runmin through the CO2, CH4, and d13CH4 concentrations (look for input areas)
+#      C. Calculate ratios
+#	D. Outputs summary information for each of the averaged columns 
+#
+#V2 - Sept 20 2014. Now includes 1 minute averaging in before runmin, general script cleaning
+	#And "seeding" of the eratio with 1/1000 the normal values of CO2 and CH4, to start it off on natural
+	#The rest will be deviation in eratio from natural 
+	#But, this technique controls the sensitivity.  Too much natural eratio, and that's a problem.
+#V3 - For New Glasgow Shiny Data
+#V4 - This version iterates to test runmins and padding - to examine laplace kurtosis
+#v5 - This version counts the number of peaks, and optimizes
+############################################################################################
